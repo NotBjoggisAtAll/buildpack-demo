3fa399c20a0fefb3a7491812ee3fc691cfbb9601
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-06-09 14:16:15 +0200

    Initial commit

---
+++ src/test/java/no/jonasandersen/buildpackdemo/BuildpackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.jonasandersen.buildpackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildpackDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
